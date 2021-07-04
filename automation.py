a7f476406f12fe7a5b8e3d5bf1238a24b42b0e0a
anudhatgithub <70832527+anudhatgithub@users.noreply.github.com>
2021-07-04 10:31:01 +0530

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,65 @@
+day = input("whats day is it?:")
+if(day == 'mon'):
+
+    print("wear the yellow shirt today with blue jeans")
+    weather = input("whats the weather today?:")
+    if(weather == "sunny"):
+        print("wear the thinner white shirt")
+    elif(weather == "cold"):
+        print("wear the yellow jacket")
+    elif(weather == "rainy"):
+        print("wear the yellow rain coat")
+elif(day == 'tue'):
+    print("wear the green shirt with brown shorts")
+    weather = input("whats the weather today?:")
+    if(weather == "sunny"):
+        print("wear the thinner white shirt")
+    elif(weather == "cold"):
+        print("wear the yellow jacket")
+    elif(weather == "rainy"):
+        print("wear the yellow rain coat")
+elif(day == 'wed'):
+    print("wear the red shirt and brown pants")
+    weather = input("whats the weather today?:")
+    if(weather == "sunny"):
+        print("wear the thinner white shirt")
+    elif(weather == "cold"):
+        print("wear the yellow jacket")
+    elif(weather == "rainy"):
+        print("wear the yellow rain coat")
+elif(day == 'thu'):
+    print("wear the maroon shirt with black jeans")
+    weather = input("whats the weather today?:")
+    if(weather == "sunny"):
+        print("wear the thinner white shirt")
+    elif(weather == "cold"):
+        print("wear the yellow jacket")
+    elif(weather == "rainy"):
+        print("wear the yellow rain coat")
+elif(day == 'fri'):
+    print('wear the white sweater with black pants')
+    weather = input("whats the weather today?:")
+    if(weather == "sunny"):
+        print("wear the thinner white shirt")
+    elif(weather == "cold"):
+        print("wear the yellow jacket")
+    elif(weather == "rainy"):
+        print("wear the yellow rain coat")
+elif(day == 'sat'):
+    print('wear the white shirt  with black pants')
+    weather = input("whats the weather today?:")
+    if(weather == "sunny"):
+        print("wear the thinner white shirt")
+    elif(weather == "cold"):
+        print("wear the yellow jacket")
+    elif(weather == "rainy"):
+        print("wear the yellow rain coat")
+elif(day == 'sun'):
+    print('wear the yellow shirt with brown pants')
+    weather = input("whats the weather today?:")
+    if(weather == "sunny"):
+        print("wear the thinner white shirt")
+    elif(weather == "cold"):
+        print("wear the yellow jacket")
+    elif(weather == "rainy"):
+        print("wear the yellow rain coat")
